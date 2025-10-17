b0c0c4d125f1e6ce26499d3364c9405adb5fb1fd
Gabriel Martins <gab.smartinn@outlook.com>
2025-10-17 16:52:55 -0300

    feat: add in-memory-orgs-repository to unit tests

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,36 @@
+import type { OrgsRepository } from '#repositories/orgs-repository'
+import type { Org, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryOgsRepository implements OrgsRepository {
+  public items: Org[] = []
+
+  async create(data: Prisma.OrgUncheckedCreateInput) {
+    const org = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      passwordHash: data.passwordHash,
+      address: data.address,
+      city: data.city,
+      zipCode: data.zipCode,
+      phone: data.phone,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+
+    this.items.push(org)
+
+    return org
+  }
+
+  async findByEmail(email: string) {
+    const org = this.items.find((item) => item.email === email)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+}
