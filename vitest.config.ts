051735b2ceccebd03807d174a807253214ef0492
Gabriel Martins <gab.smartinn@outlook.com>
2025-10-20 16:12:38 -0300

    chore: configure env variables path for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import dotenv from 'dotenv'
 import tsConfigPaths from 'vite-tsconfig-paths'
+
+dotenv.config({ path: '.env.test' })
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
