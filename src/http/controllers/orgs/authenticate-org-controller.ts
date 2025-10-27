224a448f0ccfc495110b569f2fa233cf93ef2bcc
Gabriel Martins <gab.smartinn@outlook.com>
2025-10-27 15:02:16 -0300

    feat: add authenticate-org-controller

---
+++ src/http/controllers/orgs/authenticate-org-controller.ts
@@ -0,0 +1,77 @@
+import { PrismaOrgsRepository } from '#repositories/prisma/prisma-orgs-repository'
+import { InvalidCredentialsError } from '#use-cases/errors/invalid-credentials-error'
+import { AuthenticateOrgUseCase } from '#use-cases/orgs/authenticate-org-use-case'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z, ZodError } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const orgsRepository = new PrismaOrgsRepository()
+    const authenticateOrgUseCase = new AuthenticateOrgUseCase(orgsRepository)
+
+    const { org } = await authenticateOrgUseCase.execute({
+      email,
+      password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: org.id,
+          expiresIn: '10m',
+        },
+      },
+    )
+
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: org.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return reply.status(400).send({
+        message: 'Validation error.',
+        issues: z.treeifyError(error),
+      })
+    }
+
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(401).send({
+        message: error.message,
+      })
+    }
+
+    console.log(error)
+    return reply.status(500).send({
+      message: 'Internal server error.',
+    })
+  }
+}
