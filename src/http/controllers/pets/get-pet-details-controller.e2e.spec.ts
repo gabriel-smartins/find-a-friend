e2453f0ebb86c6bcb77bc48010fccf41e683ef59
Gabriel Martins <gab.smartinn@outlook.com>
2025-10-29 17:39:09 -0300

    feat: add get-pet-details e2e test

---
+++ src/http/controllers/pets/get-pet-details-controller.e2e.spec.ts
@@ -0,0 +1,50 @@
+import { app } from '../../../app.js'
+import request from 'supertest'
+import { createAndAuthenticateOrg, createPet } from '../utils/test-helper.js'
+import { randomUUID } from 'node:crypto'
+
+describe('Get Pet Details (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get a pet details', async () => {
+    const { token } = await createAndAuthenticateOrg(app)
+    const { petId } = await createPet(app, token)
+
+    const response = await request(app.server)
+      .get(`/pets/details/${petId}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+
+    expect(response.body.pet).toEqual(
+      expect.objectContaining({
+        id: petId,
+        name: 'Rex',
+      }),
+    )
+    expect(response.body.orgContactInfo).toEqual(
+      expect.objectContaining({
+        name: 'ONG Amigo Fiel',
+        address: 'Rua dos Girassóis, 123',
+        city: 'São Paulo',
+        phone: '11987654321',
+      }),
+    )
+  })
+
+  it('should return 404 if pet is not found', async () => {
+    const nonExistentPetId = randomUUID()
+
+    const response = await request(app.server)
+      .get(`/pets/details/${nonExistentPetId}`)
+      .send()
+
+    expect(response.statusCode).toEqual(404)
+  })
+})
